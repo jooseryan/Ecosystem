7e0ee1794e211df75820c368568433da1db56c4d
jooseryan <jooseryan01@gmail.com>
2025-02-19 11:19:48 -0300

    Configurações Iniciais

---
+++ ecosystem/src/main/java/br/com/ecosystem/EcoSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoSystemApplication.class, args);
+	}
+
+}
